816e394539bc21be8cced27e4249bb840f8e443a
hrtowii <68852354+hrtowii@users.noreply.github.com>
2023-07-02 14:44:04 +0800

    refactoring code done by chatGPT (i was braindead)

--- js/cards.js
+++ js/cards.js
@@ -1,61 +1,29 @@
 const toggleModal = (modalId) => {
   const modal = document.getElementById(modalId);
-  if (modal.style.display === '') {
+  if (modal.style.display === '' || modal.style.display === 'none') {
     modal.style.display = 'flex';
   } else {
     modal.style.display = 'none';
   }
 };
 
-var subtitle = document.getElementsByClassName("card-subtitle")[0];
-var createWord = (text, index) => {
-  var word = document.createElement("span");
-  word.innerHTML = `${text}`;
-  word.classList.add("card-subtitle-word");
-  word.style.transitionDelay = `${index * 40}ms`;
-  return word;
+function createSubtitleCard(index, text, delay) {
+  var subtitle = document.getElementsByClassName("card-subtitle")[index];
+  
+  var createWord = (text, index) => {
+    var word = document.createElement("span");
+    word.innerHTML = `${text}`;
+    word.classList.add("card-subtitle-word");
+    word.style.transitionDelay = `${index * delay}ms`;
+    return word;
+  }
+
+  var addWord = (text, index) => subtitle.appendChild(createWord(text, index));
+  var createSubtitle = text => text.split(" ").map(addWord);
+  createSubtitle(text);
 }
 
-
-// code that adds to the card subtitle text. repeated 4 times for each card (this is very inefficient)
-var addWord = (text, index) => subtitle.appendChild(createWord(text, index));
-var createSubtitle = text => text.split(" ").map(addWord);
-createSubtitle("Created with others during Hack & Roll 2023. Timer app for brewing hard-to-brew coffees (V20, Aeropress)", 0);
-
-var subtitle = document.getElementsByClassName("card-subtitle")[1];
-var createWord = (text, index) => {
-  var word = document.createElement("span");
-  word.innerHTML = `${text}`;
-  word.classList.add("card-subtitle-word");
-  word.style.transitionDelay = `${index * 40}ms`;
-  return word;
-}
-
-var addWord = (text, index) => subtitle.appendChild(createWord(text, index));
-var createSubtitle = text => text.split(" ").map(addWord);
-createSubtitle("Team submission for GeekOut 2023 that won 3rd place. Centralised LMS system for JC (Holy Grail for lectures + flashcards and data visualisation)", 0);
-
-var subtitle = document.getElementsByClassName("card-subtitle")[2];
-var createWord = (text, index) => {
-  var word = document.createElement("span");
-  word.innerHTML = `${text}`;
-  word.classList.add("card-subtitle-word");
-  word.style.transitionDelay = `${index * 30}ms`;
-  return word;
-}
-
-var addWord = (text, index) => subtitle.appendChild(createWord(text, index));
-var createSubtitle = text => text.split(" ").map(addWord);
-createSubtitle("Created with others during LCH 2022. Geoguessr clone with an uncanny meme.", 0);
-
-var subtitle = document.getElementsByClassName("card-subtitle")[3];
-var createWord = (text, index) => {
-  var word = document.createElement("span");
-  word.innerHTML = `${text}`;
-  word.classList.add("card-subtitle-word");
-  word.style.transitionDelay = `${index * 40}ms`;
-  return word;
-}
-var addWord = (text, index) => subtitle.appendChild(createWord(text, index));
-var createSubtitle = text => text.split(" ").map(addWord);
-createSubtitle("Created for a school project in NYP.", 0);
+createSubtitleCard(0, "Created with others during Hack & Roll 2023. Timer app for brewing hard-to-brew coffees (V20, Aeropress)", 40);
+createSubtitleCard(1, "Team submission for GeekOut 2023 that won 3rd place. Centralised LMS system for JC (Holy Grail for lectures + flashcards and data visualisation)", 40);
+createSubtitleCard(2, "Created with others during LCH 2022. Geoguessr clone with an uncanny meme.", 30);
+createSubtitleCard(3, "Created for a school project in NYP.", 40);
